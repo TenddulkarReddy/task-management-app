6abb6e5995c75f04d3cacd3e456762190fe1254a
TenddulkarReddy <tenddulkarreddy@gmail.com>
2026-05-23 18:51:00 +0530

    fix: resolve case-sensitive pathing issue for deployment

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Task = require('../models/Task');
+const Task = require('../models/task');
 const auth = require('../middleware/auth');
 
 router.post('/', auth, async (req, res) => {
