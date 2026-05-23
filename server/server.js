ed3cefeae499ba4aae3003acb08086ab4faa8438
TenddulkarReddy <tenddulkarreddy@gmail.com>
2026-05-23 18:35:50 +0530

    chore: silence line warnings and apply clean module boundaries

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use('/api/tasks', require('./routes/taskRoutes'));
 
 const PORT = process.env.PORT || 5000;
 
-sequelize.sync({ force: false })
+sequelize.sync({ alter:true })
   .then(() => {
     console.log('Database synced successfully.');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
